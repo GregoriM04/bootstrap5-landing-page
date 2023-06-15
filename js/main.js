3873ba9cb30501e5ecd5f664f3cfdfc4cd319669
Gregori <gregorim143@gmail.com>
2023-06-14 21:55:53 -0400

    back to top functionality added

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* Back to top button functionality */
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When scrolling down 40px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When clicking on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
